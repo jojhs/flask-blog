45e860a48908736213a536c0d907fe8335726938
vjohs <vjohs@icloud.com>
2022-05-01 20:18:19 +0900

    blog/__init.py | FlaskForm이 작동하기 위한 비밀키 설정(배포시 환경변수 처리)

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,9 +6,14 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-########################
-#### DATABASE SETUP ####
-########################
+##########################
+#### SECRET_KEY SETUP ####
+##########################
+app.config['SECRET_KEY'] = 'mysecret'
+
+##########################
+#### DATABASE SETUP ######
+##########################
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,9 +21,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
-########################
-#### LOGIN CONFIGS #####
-########################
+#########################
+#### LOGIN CONFIGS ######
+#########################
 login_manager = LoginManager() 
 login_manager.init_app(app)
 login_manager.login_view = 'users.login'
