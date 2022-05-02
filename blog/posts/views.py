6fabeb504736b3a4b3dfc5650644b62dd0b5f6a7
vjohs <vjohs@icloud.com>
2022-05-02 13:51:21 +0900

    블로그 포스트 생성 VIEW

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -6,3 +6,23 @@ from blog.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
 
+# CREATE 
+@posts.route('/create', methods=['GET','POST'])
+@login_required
+def create_post(): 
+    form = PostForm()
+    
+    if form.validate_on_submit() :     
+        post = PostForm(title=form.title.data, 
+                        text=form.text.data,
+                        user_id=current_user.id) 
+        db.session.add(post)
+        db.session.commit()
+        flash('블로그 포스트가 생성 되었습니다.')
+        return redirect(url_for('core.index'))
+    
+    return render_template('create_post.html', form=form)
+    
+# BLOG POST (VIEW)
+# UPDATE 
+# DELETE 
